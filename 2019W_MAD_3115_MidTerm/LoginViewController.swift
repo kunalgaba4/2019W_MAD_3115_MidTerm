7db1fb6cca370ff4b980364086c2cb16713f6b72
mac <mac@macs-MacBook-Pro.local>
2019-03-20 18:38:38 -0400

    Login Screen created.

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var rememberMeSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func userRememberCheck(){
+        if let email = UserDefaults.standard.string(forKey: "userEmail"){
+            usernameTextField.text = email
+            if let pass = UserDefaults.standard.string(forKey: "pass"){
+                passwordTextField.text = pass
+                rememberMeSwitch.setOn(true, animated: false)
+            }else{
+                rememberMeSwitch.setOn(false, animated: false)
+            }
+        }
+    }
 
 
+    @IBAction func signInButton(_ sender: UIButton) {
+        let userOrEmail = usernameTextField.text
+        let password = passwordTextField.text
+        if (userOrEmail!.isEmpty || userOrEmail!.contains("")){
+            showAlert(title: "Error !!", message: "Please Enter Valid Email or Username")
+            return
+        }
+        
+        if (password!.isEmpty) || (password!.contains("")) {
+            showAlert(title: "Error !!", message: "Please Enter Valid Password")
+            return
+        }
+        
+        if userOrEmail == "admin" || password == "admin@123"{
+            let userdefault = UserDefaults.standard
+            if self.rememberMeSwitch.isOn {
+                userdefault.set(self.usernameTextField.text,forKey:"userEmail")
+                userdefault.set(self.passwordTextField.text, forKey: "pass")
+            }else{
+                userdefault.removeObject(forKey: "userEmail")
+                userdefault.removeObject(forKey: "pass")
+            }
+        }else{
+            showAlert(title: "Error !!", message: "Id or password is Invalid")
+            return
+        }
+    }
+    
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
 }
 
